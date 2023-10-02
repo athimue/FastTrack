a177bbe48a829f2eb2d5ee2e65b6579e149ce5dc
athimue <84283744+athimue@users.noreply.github.com>
2023-10-02 12:07:35 +0200

    feat(season): add dropdown to see the schedule of other season than the current one

--- src/screens/season/index.tsx
+++ src/screens/season/index.tsx
@@ -1,43 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, FlatList, TouchableOpacity, Linking, Image } from "react-native";
 import { useGetCurrentSeasonQuery } from "../../store/api/seasonApi";
 import { useNavigation } from "@react-navigation/native";
+import { SelectList } from "react-native-dropdown-select-list";
+import { ActivityIndicator } from "@react-native-material/core";
+
+function Loader({ isLoading }) {
+  if (isLoading) {
+    return <ActivityIndicator size="large" />;
+  }
+  return null;
+}
 
 const Season = () => {
-  const { data } = useGetCurrentSeasonQuery(null);
   const navigation = useNavigation();
+  const [season, setSeason] = useState<string>("2023");
+  const { data, isLoading } = useGetCurrentSeasonQuery(season);
+  const spinnerChoices = [
+    { key: "2023", value: "2023" },
+    { key: "2022", value: "2022" },
+    { key: "2021", value: "2021" },
+    { key: "2020", value: "2020" },
+    { key: "2019", value: "2019", disabled: true },
+    { key: "2018", value: "2018" },
+  ];
+
+  const renderItem = ({ item, index }) => {
+    const itemStyle = index % 2 === 0 ? [styles.dataView, styles.leftColumn] : [styles.dataView, styles.rightColumn];
+
+    return (
+      <TouchableOpacity style={itemStyle} onPress={() => navigation.navigate("RaceResult", { raceId: item.round })}>
+        <Text style={styles.title}>GP : {item.round}</Text>
+        <Text style={styles.subTitle}>{item.raceName}</Text>
+        <Text style={styles.text}>
+          {item.Circuit.circuitName} / {item.Circuit.Location.country}
+        </Text>
+        <Text style={styles.text}>Date : {item.date}</Text>
+        <Text style={styles.text}>Time : {item.time}</Text>
+        <Text style={styles.text} onPress={() => Linking.openURL(item.url)}>
+          Wikipedia page
+        </Text>
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <View style={[styles.container, StyleSheet.absoluteFill]}>
-      <FlatList
-        data={data?.MRData.RaceTable?.Races}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            style={styles.dataView}
-            onPress={() => navigation.navigate("RaceResult", { raceId: item.round })}
-          >
-            <Text style={styles.title}>GP : {item.round}</Text>
-            <Text style={styles.subTitle}>{item.raceName}</Text>
-            <Text style={styles.text}>
-              {item.Circuit.circuitName} / {item.Circuit.Location.country}
-            </Text>
-            <Text style={styles.text}>Date : {item.date}</Text>
-            <Text style={styles.text}>Time : {item.time}</Text>
-            <Text style={styles.text} onPress={() => Linking.openURL(item.url)}>
-              Wikipedia page
-            </Text>
-          </TouchableOpacity>
-        )}
-        numColumns={3}
-        keyExtractor={(item) => item.round}
+      <Text>PLANNING OF THE SELECTED SEASON</Text>
+      <SelectList
+        boxStyles={{ marginVertical: 20 }}
+        setSelected={(val: string) => setSeason(val)}
+        data={spinnerChoices}
+        placeholder={season}
+        searchPlaceholder={season}
+        save="value"
       />
+      {!data && <Loader isLoading={isLoading} />}
+      {data && (
+        <FlatList
+          data={data?.MRData.RaceTable.Races}
+          renderItem={renderItem}
+          numColumns={2}
+          keyExtractor={(item) => item.round}
+        />
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginBottom: 20,
+    margin: 20,
   },
   header: {
     textAlign: "center",
@@ -49,9 +82,11 @@ const styles = StyleSheet.create({
   },
   dataView: {
     flex: 1,
-    borderWidth: 1,
     padding: 5,
     margin: 5,
+    borderTopWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -65,6 +100,12 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 18,
+  },
+  leftColumn: {
+    marginTop: 10,
+  },
+  rightColumn: {
+    marginTop: 40,
   },
 });
 
