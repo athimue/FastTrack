88cb7880d73747ec87914b0a65b1cec81ab8b78b
athimue <84283744+athimue@users.noreply.github.com>
2023-10-25 10:13:44 +0200

    impr(api): add all api route

--- src/data/network/api/ErgastApi.ts
+++ src/data/network/api/ErgastApi.ts
@@ -6,6 +6,8 @@ import { injectable } from "inversify";
 @injectable()
 export class ErgastApi {
   baseUrl = "http://ergast.com/api/f1";
+
+  // RACE API
   getLastRace = async (): Promise<RaceDto> => {
     return axios.get(`${this.baseUrl}/current/last/results.json`).then((response) => {
       let raceResponseDto: RaceResponseDto = response.data;
@@ -15,6 +17,51 @@ export class ErgastApi {
 
   getNextRace = async (): Promise<RaceDto> => {
     return axios.get(`${this.baseUrl}/current/next.json`).then((response) => {
+      let raceResponseDto: RaceResponseDto = response.data;
+      return raceResponseDto.MRData.RaceTable.Races[0];
+    });
+  };
+
+  // SEASON API
+  getCurrentSeason = async (season: number): Promise<RaceDto> => {
+    return axios.get(`${this.baseUrl}/${season}.json`).then((response) => {
+      let raceResponseDto: RaceResponseDto = response.data;
+      return raceResponseDto.MRData.RaceTable.Races[0];
+    });
+  };
+
+  getRaceResults = async (season: number, raceId: number): Promise<RaceDto> => {
+    return axios.get(`${this.baseUrl}/${season}/${raceId}/results.json`).then((response) => {
+      let raceResponseDto: RaceResponseDto = response.data;
+      return raceResponseDto.MRData.RaceTable.Races[0];
+    });
+  };
+
+  getCircuits = async (season: number): Promise<RaceDto> => {
+    return axios.get(`${this.baseUrl}/${season}/circuits.json`).then((response) => {
+      let raceResponseDto: RaceResponseDto = response.data;
+      return raceResponseDto.MRData.RaceTable.Races[0];
+    });
+  };
+
+  // DRIVER API
+  getDrivers = async (): Promise<RaceDto> => {
+    return axios.get(`${this.baseUrl}/drivers.json`).then((response) => {
+      let raceResponseDto: RaceResponseDto = response.data;
+      return raceResponseDto.MRData.RaceTable.Races[0];
+    });
+  };
+
+  // STANDINGS API
+  getDriverStandings = async (season: number): Promise<RaceDto> => {
+    return axios.get(`${this.baseUrl}/${season}/driverstandings.json`).then((response) => {
+      let raceResponseDto: RaceResponseDto = response.data;
+      return raceResponseDto.MRData.RaceTable.Races[0];
+    });
+  };
+
+  getConstructorStandings = async (season: number): Promise<RaceDto> => {
+    return axios.get(`${this.baseUrl}/${season}/constructorstandings.json`).then((response) => {
       let raceResponseDto: RaceResponseDto = response.data;
       return raceResponseDto.MRData.RaceTable.Races[0];
     });
