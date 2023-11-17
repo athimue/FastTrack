43a2be69c2ee8e0c7ed3af044481a514907560b4
athimue <84283744+athimue@users.noreply.github.com>
2023-11-17 17:13:22 +0100

    impr(home): extract last race component

--- src/presentation/controller/HomeController.tsx
+++ src/presentation/controller/HomeController.tsx
@@ -1,14 +1,14 @@
 import "reflect-metadata";
 import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet, FlatList } from "react-native";
+import { Text, StyleSheet, ScrollView } from "react-native";
 import CountDown from "react-native-countdown-component";
-import { DataTable, Divider } from "react-native-paper";
 import { GetLastRaceUseCase } from "../../domain/usecase/GetLastRaceUseCase";
 import { Race } from "../../domain/model/Race";
 import moment from "moment";
 import container, { TYPES } from "../../../inversify.config";
 import { GetNextRaceUseCase } from "../../domain/usecase/GetNextRaceUseCase";
 import { NextRace } from "../component/NextRace";
+import { LastRace } from "../component/LastRace";
 
 const HomeController: React.FC = () => {
   const getLastRaceUseCase: GetLastRaceUseCase = container.get(TYPES.GetLastRaceUseCase);
@@ -40,7 +40,7 @@ const HomeController: React.FC = () => {
   }, [totalDuration, lastRace]);
 
   return (
-    <View style={{ backgroundColor: "#1e1e1e", flex: 1 }}>
+    <ScrollView style={{ backgroundColor: "#1e1e1e", flex: 1 }}>
       <Text style={styles.title}>NEXT RACE ↓</Text>
       {totalDuration && (
         <CountDown
@@ -48,7 +48,7 @@ const HomeController: React.FC = () => {
           timeToShow={["D", "H", "M", "S"]}
           onFinish={() => {}}
           onPress={() => {}}
-          size={40}
+          size={30}
           digitStyle={{ backgroundColor: "#ffffff" }}
           digitTxtStyle={{ color: "#f00201" }}
           timeLabels={{ d: "DD", h: "HH", m: "MM", s: "SS" }}
@@ -57,50 +57,8 @@ const HomeController: React.FC = () => {
         />
       )}
       <NextRace race={nextRace} />
-      <Text style={styles.title}>LAST RACE ↓</Text>
-      <View style={{ margin: 25 }}>
-        <Text style={styles.information}>
-          {lastRace?.season} {lastRace?.raceName}
-        </Text>
-        <Text style={styles.information}>
-          {lastRace?.circuit.circuitName} / {lastRace?.circuit.location.country}
-        </Text>
-        <Text style={[styles.information, { marginBottom: 10 }]}>{lastRace?.date.toString()}</Text>
-        <Divider />
-        <Text style={[styles.information, { marginTop: 10, textAlign: "center" }]}>LEADERBOARD</Text>
-        <View style={{}}>
-          <DataTable>
-            <DataTable.Header>
-              <DataTable.Title textStyle={{ color: "#ffffff" }}>Pos.</DataTable.Title>
-              <DataTable.Title textStyle={{ color: "#ffffff" }}>Name</DataTable.Title>
-              <DataTable.Title textStyle={{ color: "#ffffff" }}>Team</DataTable.Title>
-              <DataTable.Title textStyle={{ color: "#ffffff" }}>Time / Status</DataTable.Title>
-              <DataTable.Title textStyle={{ color: "#ffffff" }}>Points won</DataTable.Title>
-            </DataTable.Header>
-            <FlatList
-              data={lastRace?.results}
-              renderItem={({ item, index }) => (
-                <DataTable.Row key={index}>
-                  <DataTable.Cell textStyle={{ color: "#ffffff" }}>{item.position}</DataTable.Cell>
-                  <DataTable.Cell textStyle={{ color: "#ffffff" }}>
-                    {item.driver.givenName} {item.driver.familyName}
-                  </DataTable.Cell>
-                  <DataTable.Cell textStyle={{ color: "#ffffff" }}>{item.carConstructor.name}</DataTable.Cell>
-                  <DataTable.Cell textStyle={{ color: "#ffffff" }}>
-                    {item.time?.time ? item.time?.time : item.status}
-                  </DataTable.Cell>
-                  <DataTable.Cell textStyle={{ color: "#ffffff" }}>
-                    {Number(item.points) > 0 ? "+" + item.points : "-"}
-                  </DataTable.Cell>
-                </DataTable.Row>
-              )}
-              scrollEnabled={true}
-              keyExtractor={(driverStanding) => driverStanding.position}
-            />
-          </DataTable>
-        </View>
-      </View>
-    </View>
+      <LastRace lastRace={lastRace} />
+    </ScrollView>
   );
 };
 
@@ -109,7 +67,7 @@ const styles = StyleSheet.create({
     fontSize: 30,
     textAlign: "center",
     color: "#707079",
-    padding: 20,
+    padding: 10,
     fontWeight: "bold",
   },
   information: {
